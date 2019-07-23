07ca22167314850c341a2b717f67befa12483749
xiaoweimei <1227380089@qq.com>
2019-07-23 23:57:17 +0800

    v3

--- src/App.js
+++ src/App.js
@@ -9,25 +9,27 @@ class App extends React.Component {
         你点击了<span id="value">{this.props.n}</span>次
         <div>
           <button id="add1" onClick={()=>this.props.add1()}>+1</button>
-          <button id="add2" >+2</button>
+          <button id="add2" onClick={()=>this.props.add2()}>+2</button>
           <button id="addIfOdd" >如果是单数就+1</button>
-          <button id="add1After2Sec" >两秒钟后+1</button>
+          <button id="add1After2Sec" onClick={()=>this.props.add4()}>两秒钟后+1</button>
         </div>
       </div>
     );
   }
 }
 
-function getPartialStore(state){
+function mapStateToProps(state){
   return {
     n:state.n
   }
 }
-const y={
-  add1:()=>{
-    return {type:'add',payload:1}
+function mapDispatchToProps(dispatch){
+  return {
+    add1:()=>dispatch({type:'add',payload:1}),
+    add2:()=>dispatch({type:'add',payload:2}),
+    add4:()=>setTimeout(()=>dispatch({type:'add',payload:1}),2000)
   }
 }
 
 
-export default connect(getPartialStore,y)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
