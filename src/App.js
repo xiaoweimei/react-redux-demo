c1a8552431c3c5b7164526dfb9c80c76d4b5d44d
xiaoweimei <1227380089@qq.com>
2019-07-23 21:45:59 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
+import {connect} from "react-redux"
 
 class App extends React.Component {
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  add3(){
-    this.props.onAdd3()
-  }
-  add4(){
-    this.props.onAdd4()
-  }
+
   render(){
     return (
       <div className="App">
-        你点击了<span id="value">{this.props.value}</span>次
+        你点击了<span id="value">{this.props.n}</span>次
         <div>
-          <button id="add1" onClick={()=>this.add1()}>+1</button>
-          <button id="add2" onClick={()=>this.add2()}>+2</button>
-          <button id="addIfOdd" onClick={()=>this.add3()}>如果是单数就+1</button>
-          <button id="add1After2Sec" onClick={()=>this.add4()}>两秒钟后+1</button>
+          <button id="add1" onClick={()=>this.props.add1()}>+1</button>
+          <button id="add2" >+2</button>
+          <button id="addIfOdd" >如果是单数就+1</button>
+          <button id="add1After2Sec" >两秒钟后+1</button>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+function getPartialStore(state){
+  return {
+    n:state.n
+  }
+}
+const y={
+  add1:()=>{
+    return {type:'add',payload:1}
+  }
+}
+
+
+export default connect(getPartialStore,y)(App);
